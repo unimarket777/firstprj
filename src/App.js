b0d93d6a9e7921b3337a94d5aaeefc90d0a17543
Tolstov Pavel <faworit296@yandex.ru>
2020-03-30 20:53:43 +0300

    version 1.0

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('А родители разрешили?');
+  }
+}
+
+let age = prompt('Сколько вам лет?', 18);
+
+if ( checkAge(age) ) {
+  alert( 'Доступ получен' );
+} else {
+  alert( 'Доступ закрыт' );
+}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ashot otday gewachku
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
